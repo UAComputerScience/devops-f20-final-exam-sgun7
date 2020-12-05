b2a82202a15c6c8dad50f9a8da77aa4927368b67
sgun7 <srg97@zips.uakron.edu>
2020-12-05 17:28:05 -0500

    Add in markturn files

--- jenkinsPipeline.groovy
+++ jenkinsPipeline.groovy
@@ -8,9 +8,10 @@ agent
     }
 }
 stages {
+    //TODO will he be able to access this because this is my branch
     stage('Setup'){
         steps{
-        //sh 'cat /etc/os-release'
+        sh 'cat /etc/os-release'
         //git credentialsId: 'd68d2692-a1ef-4f38-aa74-c96014acbf00', url: 'https://github.com/UAComputerScience/markturn-sgun7.git'
         git branch: 'main', credentialsId: 'GitHub', url: 'https://github.com/UAComputerScience/markturn-sgun7.git'
         //sh 'git clone https://sgun7@github.com/UAComputerScience/markturn-sgun7.git'
@@ -23,15 +24,27 @@ stages {
            cmakeBuild buildDir: 'build', cmakeArgs: '-DLINK_STATIC=0FF', generator: 'Ninja', installation: 'cmake'
         }
     }
-    stage('Build')
+    stage('Build Generation')
     {
         steps{
            sh 'cd build; ninja clean; ninja' 
         }
     }
 
-
-
+    stage('Test')
+    {
+        steps{
+        //ctest arguments: '-R json2xml_t', installation: 'cmake', workingDir: 'build'
+        ctest installation: 'cmake', workingDir: 'build'
+        }
+    }
+    stage('Package')
+    {
+        steps{
+            //TODO is this correct
+           cpack arguments: '-G DEB', installation: 'cmake', workingDir: 'build'
+        }
+    }
 
 }
 }
