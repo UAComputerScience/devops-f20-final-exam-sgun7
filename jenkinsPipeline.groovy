a092bd6442575daaed4eae4302a6bba76a131dc1
sgun7 <srg97@zips.uakron.edu>
2020-12-06 16:26:44 -0500

    Formatting and archive added

--- jenkinsPipeline.groovy
+++ jenkinsPipeline.groovy
@@ -1,50 +1,54 @@
-pipeline{
+pipeline {
 
-agent 
-{
-    docker {
-        image 'ub2-new'
-        args '--user root'
-    }
-}
-stages {
-    //TODO will he be able to access this because this is my branch
-    stage('Setup'){
-        steps{
-        sh 'cat /etc/os-release'
-        //git credentialsId: 'd68d2692-a1ef-4f38-aa74-c96014acbf00', url: 'https://github.com/UAComputerScience/markturn-sgun7.git'
-        git branch: 'main', credentialsId: 'GitHub', url: 'https://github.com/UAComputerScience/markturn-sgun7.git'
-        //sh 'git clone https://sgun7@github.com/UAComputerScience/markturn-sgun7.git'
-        //echo 'Hello world'
-        }      
-    }
-    stage('CMake')
-    {
-        steps{
-           cmakeBuild buildDir: 'build', cmakeArgs: '-DLINK_STATIC=0FF', generator: 'Ninja', installation: 'cmake'
+    agent {
+        docker {
+            image 'srg97/markturn_ubuntu'
+            args '--user root'
         }
     }
-    stage('Build Generation')
-    {
-        steps{
-           sh 'cd build; ninja clean; ninja' 
+    stages {
+        stage('Setup'){
+            steps{
+                sh 'cat /etc/os-release'
+                sh 'curl -L https://github.com/UAComputerScience/devops-f20-final-exam-sgun7/archive/v1.0.0.tar.gz | tar xz'
+                sh 'mv devops-f20-final-exam-sgun7-1.0.0 /Source'
+            }      
         }
-    }
+        stage('CMake'){
+            steps{
+                //cmakeBuild buildDir: 'build', cmakeArgs: '-DLINK_STATIC=0FF', generator: 'Ninja', installation: 'cmake'
+                sh 'mkdir /Build'
+                sh 'cd /Build; cmake /Source -DLINK_STATIC=0FF -G Ninja'
+            }
+        }
+        stage('Build Generation'){
+            steps{
+                //sh 'cd /build; ninja clean; ninja' 
+                sh 'cd /Build; ninja'
+            }
+        }
+        stage('Test'){
+            steps{
+                //ctest arguments: '-R json2xml_t', installation: 'cmake', workingDir: 'build'
+                //ctest installation: 'cmake', workingDir: 'build'
+                sh 'cd /Build; ctest'
+                }
+        }
+        stage('Package'){
+            steps{
+                //TODO is this correct
+                // cpack arguments: '-G DEB', installation: 'cmake', workingDir: 'build'
+                sh 'cd /Build ; cpack -G DEB'
+            }
+        }
+        stage('Archive'){
+            steps{
+                sh 'cd /Build; ls'
+                archiveArtifacts artifacts: 'build/markturn-0.0.0-Linux.deb', onlyIfSuccessful: true
+            }
+        }
 
-    stage('Test')
-    {
-        steps{
-        //ctest arguments: '-R json2xml_t', installation: 'cmake', workingDir: 'build'
-        ctest installation: 'cmake', workingDir: 'build'
-        }
-    }
-    stage('Package')
-    {
-        steps{
-            //TODO is this correct
-           cpack arguments: '-G DEB', installation: 'cmake', workingDir: 'build'
-        }
-    }
+}
 
 }
-}
+
